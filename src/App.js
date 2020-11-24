db68d92e21238f1b01e79b2ad62af2c729f15cc3
Roger Campbell II <rc1336@gmail.com>
2020-11-23 23:18:24 -0500

    Imported data and able to iterate over json data. Also able to render deck names. NEXT STEP: Pass down deck data to a deck component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { stringify } from 'json5';
 import './App.css';
+import Card from './Card'
+
+import cahCards from './cah-cards-full.json'
+
+let deconstructDecksObject = Object.entries(cahCards).map(([key, value]) => value)
+
+let decks = deconstructDecksObject.map(deck => deck)
+
+let deckList = decks.map(deck => (
+  <Card deck={deck} />
+))
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {deckList}
     </div>
   );
 }
